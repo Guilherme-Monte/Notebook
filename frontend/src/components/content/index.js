df6a57d5ea2b4f630faee4bfc9e1ebfa70ea13c6
GuilhermeM314 <guimontenegro@hotmail.com.br>
2021-01-29 14:30:32 -0300

    Problem from API request solved

--- frontend/src/components/content/index.js
+++ frontend/src/components/content/index.js
@@ -3,24 +3,35 @@ import "./index.css";
 import { api } from "../../services/api";
 
 const Content = () => {
-    const [results, setResults] = useState([]);
+  const [results, setResults] = useState([]);
 
-    React.useEffect(() => {
-        getDbInfo();
-    }, []);
+  React.useEffect(() => {
+    getDbInfo();
+  }, []);
 
-    async function getDbInfo() {
-        const response = await api.get("/users");
-        console.log(response.data);
-        setResults(response.data);
-    }
+  async function getDbInfo() {
+    const response = await api.get("/users");
+    // console.log(response.data);
+    setResults(response.data);
+  };
 
+  const renderUserInfo = results.map((user) => {
+    // Warning each list element must have a unique key prop: https://reactjs.org/docs/lists-and-keys.html#keys
     return (
-        <div>
-            <h1>Content</h1>
-            <h2>{results[0].id}</h2>
-        </div>
+      <ul>
+        <li id={user.id} key={user.id}>Id: {user.id}</li>
+        <li id={user.name} key={user.name}>Name: {user.name}</li>
+        <li id={user.email} key={user.email}>Email: {user.email}</li>
+      </ul>
     )
+  });
+
+  return (
+    <div id="content">
+      <h1>Content</h1>
+      <h2>{renderUserInfo}</h2>
+    </div>
+  )
 }
 
 export default Content;
