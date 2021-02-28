3ea4321521ac979a80ec4be444fe55cfc37e3829
GuilhermeM314 <guimontenegro@hotmail.com.br>
2021-02-28 16:21:53 -0300

    Footer styling and statistics database update

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import "./index.css";
+import { DiGithubBadge } from "react-icons/di";
+import { SiLinkedin, SiGmail } from "react-icons/si";
 
 const Footer = () => {
     return (
-        <div id="footer">Footer</div>
+        <div id="footer">
+            <div>Made by <span id="my-name">Guilherme Montenegro</span></div>
+            <span id="socials">
+                Check out my socials here:
+                <a href="https://github.com/Guilherme-Monte" target="_blank" rel="noreferrer">
+                    <DiGithubBadge className="icons" />
+                </a>
+                <a href="https://www.linkedin.com/in/guilherme-montenegro-4a0050204/" target="_blank" rel="noreferrer">
+                    <SiLinkedin className="icons" />
+                </a>
+                <SiGmail className="icons" />= guilhermemonte.dev@gmail.com
+            </span>
+        </div>
     )
 }
 
