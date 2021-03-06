3b77592b4af84a6b95e892f5e5339cfa7df3a64e
GuilhermeM314 <guimontenegro@hotmail.com.br>
2021-03-06 18:48:37 -0300

    Bright mode 1.0

--- frontend/src/components/sidebar/index.js
+++ frontend/src/components/sidebar/index.js
@@ -1,14 +1,37 @@
 import React, { useContext, useState } from 'react';
 import "./index.css";
 import { SidebarButtonContext } from '../../SidebarButtonContext';
-import { FaFolderPlus, FaSun, FaArrowLeft } from "react-icons/fa";
+import { FaFolderPlus, FaSun, FaArrowLeft, FaMoon } from "react-icons/fa";
 
 const Sidebar = () => {
   const toggleInputs = useContext(SidebarButtonContext);
   const [buttonNameSwitcher, setButtonNameSwitcher] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   const switchName = () => {
     setButtonNameSwitcher(!buttonNameSwitcher);
+  }
+
+  const brightModeSwitcher = () => {
+    document.documentElement.style.setProperty('--text-primary', '#009dec');
+    document.documentElement.style.setProperty('--text-secondary', '#003b59');
+    document.documentElement.style.setProperty('--hover-color', '#2e86de');
+    document.documentElement.style.setProperty('--bg-primary', '#daf2fe');
+    document.documentElement.style.setProperty('--bg-secondary', '#002639');
+    document.documentElement.style.setProperty('--bg-main', '#a1dfff');
+
+    setDarkMode(false);
+  }
+
+  const darkModeSwitcher = () => {
+    document.documentElement.style.setProperty('--text-primary', '#b6b6b6');
+    document.documentElement.style.setProperty('--text-secondary', '#ececec');
+    document.documentElement.style.setProperty('--hover-color', '#a55eea');
+    document.documentElement.style.setProperty('--bg-primary', '#23232e');
+    document.documentElement.style.setProperty('--bg-secondary', '#141418');
+    document.documentElement.style.setProperty('--bg-main', '#2f3542');
+
+    setDarkMode(true);
   }
 
   return (
@@ -19,10 +42,17 @@ const Sidebar = () => {
           {buttonNameSwitcher ? "Back" : "New note"}
         </span>
       </div>
-      <div className="nav-item">
-        <FaSun className="icons" />
-        <span className="link-text">Themify</span>
-      </div>
+      { darkMode ?
+        <div className="nav-item">
+          <FaSun className="icons" />
+          <span className="link-text" onClick={brightModeSwitcher}>Bright</span>
+        </div>
+        :
+        <div className="nav-item">
+          <FaMoon className="icons" />
+          <span className="link-text" onClick={darkModeSwitcher}>Dark</span>
+        </div>
+      }
     </div>
   )
 }
